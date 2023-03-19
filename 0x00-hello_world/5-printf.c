53341b65b67794b55cc08097bb3526297701c531
mungathimu <mungathiamugendi@gmail.com>
2023-03-18 19:18:41 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main
+ *
+ * Return: It should Always be o
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
