f3904bdaaa08fea6ff766f6d3fd58f94c23f5774
mungathimu <mungathiamugendi@gmail.com>
2023-03-22 17:30:23 -0700

    Alphabets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - entry point
- *
- * Return: always 0
+ * main -entry point
+ * _putchar: prints the alphabet putchar
+ * Return: Always 0
  */
-int main(void)
-	printf("_putchar \n");
 
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+
+}
