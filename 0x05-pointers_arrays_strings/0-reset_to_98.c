0a18054cdf9e0ab07c33f02200c3917ebac207da
mungathimu <mungathiamugendi@gmail.com>
2023-03-27 11:57:20 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value of n is 98
+ * Return: 0
+ * @n: is the value that will be replaceed
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ pointers, strinngs and arrays
