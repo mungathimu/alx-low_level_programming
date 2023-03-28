fba1e20be64ca7e25ec9d89dc7e0ff6b60d10d06
mungatghimu <mungathiamugendi@gmail.com>
2023-03-28 13:53:56 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,18 @@
 #include "main.h"
+/**
+ * _strcpy - copies string pointed to by src and including the null terminator
+ * @dest: Where string will be copied to
+ * @src: Holds the string
+ * Return: dest
+ */
 
 char *_strcpy(char *dest, char *src)
 {
+	int i;
 
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	return (dest);
 
 
 
