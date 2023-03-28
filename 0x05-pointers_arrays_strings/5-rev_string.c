7d9ebc2458f49d90890abf6b65ad254b96e418b9
mungatghimu <mungathiamugendi@gmail.com>
2023-03-27 23:35:59 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - this is a function for reverses a string
+ * @s: function parameter
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+
+		count++;
+
+	for (i = count ; i >= 0 ; --i)
+
+
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: this is the function parameter
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
