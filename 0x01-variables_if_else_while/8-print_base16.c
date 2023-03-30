a946c5735780ec69833b32edef758a77e4a32c29
mungatghimu <mungathiamugendi@gmail.com>
2023-03-29 17:46:27 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - this is where my actual code is
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	char l;
+
+	for (j = 0 ; j < 10; j++)
+		putchar(j + '0');
+	for (l = 'a' ; l <= 'f' ; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
