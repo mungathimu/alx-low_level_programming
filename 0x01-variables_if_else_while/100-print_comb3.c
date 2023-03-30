0052e8c01784e8e0a9444c6181211e1ea2df2b17
mungatghimu <mungathiamugendi@gmail.com>
2023-03-30 08:11:52 -0700

    different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
 /**
-  * main - This is the code will be
-  * Return: 0
-  */
+ * main - This is the code will be
+ * Return: 0
+ */
 int main(void)
 {
+	int g;
 
+	int m;
 
+	for (g = 0 ; g < 10 ; g++)
+	{
+		for (m = 1 ; m < 10 ; m++)
+		{
+			if (g < m && g != m)
+			{
+				putchar(g + '0');
+				putchar(m + '0');
 
+				if (g + m != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
+	putchar('\n');
+	return (0);
 }
+
+
+
