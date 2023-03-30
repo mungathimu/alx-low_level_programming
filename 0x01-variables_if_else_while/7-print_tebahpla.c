2b67b2b24073842c51ccad8c45dd43ddbe726aec
mungatghimu <mungathiamugendi@gmail.com>
2023-03-29 17:37:34 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z' ; j >= 'a' ; j--)
+		putchar(j);
+	putchar('\n');
+		return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
