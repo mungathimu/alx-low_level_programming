54be2bf4344ccd8094c83ea29015101f0c915c84
mungatghimu <mungathiamugendi@gmail.com>
2023-03-29 17:15:33 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+		putchar(k + '0');
+	putchar('\n');
+
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
