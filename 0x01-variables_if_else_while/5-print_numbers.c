823e842d1f2ea7d1cae5a7b07489b1ebeb757a5b
mungatghimu <mungathiamugendi@gmail.com>
2023-03-29 17:02:55 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j  < 10  ; j++)
+		printf("%d", j);
+	printf("\n");
+		return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
