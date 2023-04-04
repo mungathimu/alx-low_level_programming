5a22a98dca444a872f2d018c61bbd4c24070e455
mungatghimu <mungathiamugendi@gmail.com>
2023-04-04 11:29:24 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - fuction that prints 0 t0 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = '0' ; j <= '9' ; j++)
+
+		_putchar(j);
+	_putchar('\n');
+
+
+
+
+
+
+}
