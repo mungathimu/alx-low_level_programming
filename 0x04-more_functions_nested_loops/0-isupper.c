a798ff16556f55d0120e72662c2c662ce091df70
mungatghimu <mungathiamugendi@gmail.com>
2023-04-04 10:48:58 -0700

    Uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase
+ * otherwise 0
+ */
+
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c  <= 'Z')
+
+
+	{
+		return (1);
+
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
